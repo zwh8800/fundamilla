f279b648b235d3660a295299d2cae7831a437935
Zhang Weihu <496781108@qq.com>
2016-02-17 16:41:31 +0800

    f

--- md5/md5.go
+++ md5/md5.go
@@ -10,18 +10,18 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Fprint(os.Stderr, "Usage: md5 filename\n")
+		fmt.Fprintf(os.Stderr, "Usage: md5 filename\n")
 		return
 	}
 	filename := os.Args[1]
 	file, err := os.Open(filename)
 	if err != nil {
-		fmt.Fprint(os.Stderr, "os.Open: %s\n", err)
+		fmt.Fprintf(os.Stderr, "os.Open: %s\n", err)
 		return
 	}
 	hasher := md5.New()
 	if _, err := io.Copy(hasher, file); err != nil {
-		fmt.Fprint(os.Stderr, "io.Copy: %s\n", err)
+		fmt.Fprintf(os.Stderr, "io.Copy: %s\n", err)
 		return
 	}
 
